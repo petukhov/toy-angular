c828e90c4c1759aa5257581bc29cc6b7da641744
petukhov <petukhovg@gmail.com>
2024-02-13 02:22:45 +0800

    added Component decorator

--- dev/components/app.component.ts
+++ dev/components/app.component.ts
@@ -1,5 +1,12 @@
+import { Component } from 'toy-angular';
+
+@Component({
+    selector: "app-root",
+    template: "<h1>My First Angular App</h1>",
+    
+})
 export class AppComponent {
-    constructor() {
-        console.log('AppComponent');
-    }
+  constructor() {
+    console.log("AppComponent");
+  }
 }
--- framework/index.ts
+++ framework/index.ts
@@ -1,3 +1,14 @@
-export function bootstrap() {
-    console.warn('Bootstrapping...');
+export function bootstrap(component) {
+  console.warn("Bootstrapping...", component);
+}
+
+export function Component(options: {
+  selector: string;
+  template: string;
+}): ClassDecorator {
+  return function (target: any) {
+    // Add the selector and template properties to the component class
+    target.prototype.selector = options.selector;
+    target.prototype.template = options.template;
+  };
 }
